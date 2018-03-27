1fa9398d27726a8a2c301c0d2f96a0bad661f306
LiuHeng <ai747957633@qq.com>
2018-03-27 16:41:13 +0800

    add chapter2

--- main.js
+++ main.js
@@ -14,16 +14,30 @@ const SHA256 = require('crypto-js/SHA256');
 *  hash: 当前块的hash. 256bit的摘要，以一个64位16进制数呈现
 */
 class Block{
-    constructor(index, timestamp, data, previousHash = ''){
+    constructor(index, timestamp, data, previousHash = '', nonce=0){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = nonce;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data).toString() + this.previousHash).toString();
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data).toString() + this.previousHash + this.nonce).toString();
+    }
+
+    /**
+     *  proof of work
+     *  工作量证明
+     */
+    mineBlock(difficulty){
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
@@ -39,22 +53,23 @@ class Blockchain {
     //构造函数，生成创世块
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
-
 
     createGenesisBlock(){
         return new Block(0, "01/01/2018", "Genesis block", '0');
     }
 
     getLatestBlock(){
-        return  this.chain[this.chain.length - 1];
+        return this.chain[this.chain.length - 1];
     }
 
     // newBlock 传入一个区块对象
     addBlock(newBlock){
         // 重写previousHash 和 hash
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        // newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
 
         //将区块追回到当前区块链
         this.chain.push(newBlock);
@@ -78,9 +93,8 @@ class Blockchain {
     }
 }
 
-// 实例化一个区块链
+// 实例化一个区块链, 并创建一个创世区块
 let testCoin = new Blockchain();
-
 
 // 添加两个区块
 testCoin.addBlock(new Block(1, "10/01/2018", { amount: 4 }));
@@ -89,10 +103,10 @@ testCoin.addBlock(new Block(2, "12/01/2018", { amount: 10 }));
 // 打印区块链数据
 console.log(JSON.stringify(testCoin,'',4));
 
-// 验证区块链是否有效, 结果为true
-console.log('Is blockchain valid？ ' + testCoin.isChainValid());
-
-// 修改区块数据
-testCoin.chain[1].data = { amount: 100 };
-// 验证区块链是否有效, 结果为false
-console.log('Is blockchain valid？ ' + testCoin.isChainValid());
+// // 验证区块链是否有效, 结果为true
+// console.log('Is blockchain valid？ ' + testCoin.isChainValid());
+//
+// // 修改区块数据
+// testCoin.chain[1].data = { amount: 100 };
+// // 验证区块链是否有效, 结果为false
+// console.log('Is blockchain valid？ ' + testCoin.isChainValid());
